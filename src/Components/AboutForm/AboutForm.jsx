035020a09540c51f42f719327bbfed91eaaf477d
jackstasiulis <jackstasiulis@gmail.com>
2024-02-02 14:01:25 -0500

    add radio button email subject change to form

--- src/Components/AboutForm/AboutForm.jsx
+++ src/Components/AboutForm/AboutForm.jsx
@@ -1,10 +1,24 @@
 import './AboutForm.scss'
 import { useForm } from 'react-hook-form'
 import emailjs from 'emailjs-com';
+import { useEffect, useState } from 'react';
 
 const AboutForm = () => {
 
-  function sendEmail(e) {
+  
+  const options = ['General', 'Item Specific', 'Order Status', 'Collab']
+  
+  const [selectedSubject, setSelectedSubject] = useState();
+  
+  const handleSubjectChange = (subject) => {
+    setSelectedSubject(subject);
+  }
+  
+  
+  
+  
+  
+  const sendEmail = (e) => {
     e.preventDefault();  
 
     emailjs.sendForm('service_bypcokg', 'template_9w09jae', e.target, '996iLhn-7GkMbs3nv')
@@ -12,10 +26,16 @@ const AboutForm = () => {
         e.target.name.value = '';
         e.target.email.value = '';
         e.target.message.value = '';
+        e.target.emailSubject.value = ''
       }, (error) => {
           console.log(error.text);
       });
   }
+
+
+  // useEffect(() => {
+  //   console.log(selectedSubject)
+  // })
 
   return (
     <>
@@ -27,6 +47,30 @@ const AboutForm = () => {
 
               <label>name</label>
               <input type="text" name='name' />
+              
+              <div className='about-form__radio--container'>
+                {options.map((subject) => (
+                  <label 
+                  key={subject} 
+                  className= {`about-form__radio--label ${subject === selectedSubject ? 'about-form__radio--label__selected' : '' } `}>
+                      <input 
+                      className='about-form__radio'
+                      type="radio"
+                      value={subject}
+                      checked={subject === selectedSubject}
+                      onChange={ () => handleSubjectChange(subject)}
+                      name='emailSubject'
+                      />
+                      {subject}
+                  </label>
+                ))}
+               
+              </div>
+
+
+              
+
+
 
               <label>message</label>
               <input type="text" name='message' />
