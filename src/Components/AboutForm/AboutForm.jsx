632419c06228bc24d0f6d40624684083a69d8b0a
jackstasiulis <jackstasiulis@gmail.com>
2024-02-03 00:51:31 -0500

    about form error handling and error states

--- src/Components/AboutForm/AboutForm.jsx
+++ src/Components/AboutForm/AboutForm.jsx
@@ -1,84 +1,61 @@
-import './AboutForm.scss'
-import { useForm } from 'react-hook-form'
+import './AboutForm.scss';
+import { useForm } from 'react-hook-form';
 import emailjs from 'emailjs-com';
 import { useEffect, useState } from 'react';
 
 const AboutForm = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const options = ['General', 'Item Specific', 'Order Status', 'Collab'];
+  const [selectedSubject, setSelectedSubject] = useState(options[0]);
 
-  
-  const options = ['General', 'Item Specific', 'Order Status', 'Collab']
-  
-  const [selectedSubject, setSelectedSubject] = useState();
-  
   const handleSubjectChange = (subject) => {
     setSelectedSubject(subject);
   }
-  
-  
-  
-  
-  
-  const sendEmail = (e) => {
-    e.preventDefault();  
+
+  const sendEmail = (data, e) => {
+    e.preventDefault();
 
     emailjs.sendForm('service_bypcokg', 'template_9w09jae', e.target, '996iLhn-7GkMbs3nv')
       .then((result) => {
-        e.target.name.value = '';
-        e.target.email.value = '';
-        e.target.message.value = '';
-        e.target.emailSubject.value = ''
+        e.target.reset();
+        setSelectedSubject(null);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
   }
 
-
-  // useEffect(() => {
-  //   console.log(selectedSubject)
-  // })
-
   return (
-    <>
-      <div className='about-form__container'>
-        <h2 className='about-form__title'>General Inquiry Form</h2>
-          <form className='about-form' onSubmit={sendEmail}>
-              <input className='about-form__input' placeholder='EMAIL' type="text" name='email' />
+    <div className='about-form__container'>
+      <h2 className='about-form__title'>General Inquiry Form</h2>
+      <form className='about-form' onSubmit={handleSubmit(sendEmail)}>
 
-              <input className='about-form__input' placeholder='NAME' type="text" name='name' />
-              
-              <div className='about-form__radio--container'>
-                {options.map((subject) => (
-                  <label 
-                  key={subject} 
-                  className= {`about-form__radio--label ${subject === selectedSubject ? 'about-form__radio--label__selected' : '' } `}>
-                      <input 
-                      className='about-form__radio'
-                      type="radio"
-                      value={subject}
-                      checked={subject === selectedSubject}
-                      onChange={ () => handleSubjectChange(subject)}
-                      name='emailSubject'
-                      />
-                      {subject}
-                  </label>
-                ))}
-               
-              </div>
-
-
-              
-
-
-
-              <textarea className='about-form__textarea' placeholder='MESSAGE' type="text" name='message' />
-
-              <input className='about-form__submit' type="submit" />
-          </form>
-      </div>
-
-
-    </>
-  )
+        <div className='about-form__input--container'>
+          <input className={`about-form__input ${errors.email ? 'error' : ''}`} placeholder='EMAIL' type="text" {...register("email", { required: true, pattern: /^\S+@\S+\.\S+$/ })} />
+          {errors.email && <span className='about-form__input--errorAsterisk'>*</span>}
+        </div>
+        
+        <div className='about-form__input--container'>
+          <input className={`about-form__input ${errors.name ? 'error' : ''}`} placeholder='NAME' type="text" {...register("name", { required: true })} />
+          {errors.name && <span className='about-form__input--errorAsterisk'>*</span>}
+        </div>
+        
+        <div className='about-form__radio--container'>
+          {options.map((subject) => (
+            <label key={subject} className={`about-form__radio--label ${subject === selectedSubject ? 'about-form__radio--label__selected' : ''}`}>
+              <input className='about-form__radio' type="radio" value={subject} checked={subject === selectedSubject} onChange={() => handleSubjectChange(subject)} name='emailSubject' />
+              {subject}
+            </label>
+          ))}
+        </div>
+        
+        <div className='about-form__input--container'>
+          <textarea className={`about-form__textarea ${errors.message ? 'error' : ''}`} placeholder='MESSAGE' type="text" name='message' {...register("message", { required: true })} />
+          {errors.message && <span className='about-form__input--errorAsterisk'>*</span>}
+          </div>
+        <input className='about-form__submit' type="submit" />
+      </form>
+    </div>
+  );
 }
 
-export default AboutForm
+export default AboutForm;
